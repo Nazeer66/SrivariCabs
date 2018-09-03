1efe1dc2f0e4f92d59ea6860b466510685454f15
Nazeer <jemsynazeer6@gmail.com>
2018-09-03 18:06:15 +0530

    Added everthing in home page itself

--- app/js/controller.js
+++ app/js/controller.js
@@ -1,11 +1,19 @@
 app.controller("myCtrl", function($scope, $http){
     
     
-    $http.get("api/india.json").then(successCallback);
+    // $http.get("api/india.json").then(successCallback);
+
+    // function successCallback(response){
+    //     $scope.indiaData = response.data.data;
+    //     console.log("India", response.data.data);
+    // }
+
+
+    $http.get("api/data.json").then(successCallback);
 
     function successCallback(response){
-        $scope.indiaData = response.data.data;
-        console.log("India", response.data.data);
+        $scope.myData = response.data.data;
+        console.log("myData", response.data.data);
     }
 
 
